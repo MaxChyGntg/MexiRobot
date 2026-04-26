2eafd8300c8fed14850439d7f35ee83792935fd7
MaxChy <sportznoah@gmail.com>
2026-04-26 22:06:52 +0700

    Refactor bot function to memixBot and improve output

--- bot.py
+++ bot.py
@@ -1,54 +1,82 @@
 from mt5linux import MetaTrader5
 import time
 import datetime
+import os
 
-# init mt5
+# Init MetaTrader5
 mt5 = MetaTrader5()
 
+# Function Robot Memix
 
-def bot():
+
+def memixBot():
     if not mt5.initialize():
-        print("Robot Failed to Connect")
+        print("Failed To Connect -Memix Robot v0.1")
         return
     else:
-        print("----------------------------")
-        print("Welcome To Mexi Robot v0.1")
-        print("----------------------------")
+        print("\n==============================")
+        print("\n|WELCOME TO MEMIX ROBOT v0.1|")
+        print("\n=============================")
+    symbols = {1: "USDJPY", 2: "EURUSD", 3: "USDCNH", 4: "GBPUSD"}
+    howlong_default = 2
     try:
-        usrinpt = int(input("Pilih Mata Uang (USDJPY): 1, (EURUSD): 2, (USDCNH): 3,:"))
         while True:
-            if usrinpt == 1:
-                tick = mt5.symbol_info_tick("USDJPY")
-                if tick:
-                    print(
-                        f"Harga USDJPY Terbaru pertanggal ",
-                        datetime.datetime.now().date(),
-                        f"| Bid : {tick.bid} | Ask : {tick.ask}",
+            howlong_delay = input(
+                "How many second you want for the delay to the new data? (Default = 2 second) :"
+            )
+            if howlong_delay.strip() == "":
+                howlong = howlong_default
+            else:
+                try:
+                    howlong = int(howlong_delay)
+                except ValueError:
+                    print("Invalid Numbber using default 2sec")
+                    howlong = howlong_default
+            try:
+                userinput = int(
+                    input(
+                        "Choose 1 OF 4, Most Popular Currency for Info: 1:USDJPY, 2:EURUSD, 3:USDCNH, 4:GBPUSD: "
                     )
-                time.sleep(2)
-            if usrinpt == 2:
-                tick = mt5.symbol_info_tick("EURUSD")
-                if tick:
-                    print(
-                        f"Harga EURUSD Terbaru pertanggal ",
-                        datetime.datetime.now().date(),
-                        f"| Bid : {tick.bid} | Ask : {tick.ask}",
-                    )
-                time.sleep(2)
-            if usrinpt == 3:
-                tick = mt5.symbol_info_tick("USDCNH")
-                if tick:
-                    print(
-                        f"Harga USDCNH Terbaru pertanggal ",
-                        datetime.datetime.now().date(),
-                        f"| Bid : {tick.bid} | Ask : {tick.ask}",
-                    )
-                time.sleep(2)
+                )
+                if userinput in symbols:
+                    symbols_name = symbols[userinput]
+                    break
+                else:
+                    print("Invalid Number")
+            except ValueError:
+                print("Please Choose a Number")
+
+            # Main Logic
+        print(f"Checking The Data of {symbols_name}... ctrl+C to End this Program..")
+        while True:
+            tick = mt5.symbol_info_tick(f"{symbols_name}")
+            if tick:
+                print(
+                    f"Todays date : {datetime.datetime.now().date()} | Bid : {
+                        tick.bid
+                    } | Ask : {tick.ask}"
+                )
+            else:
+                print("Cannot Connect to Server")
+                break
+            time.sleep(howlong)
     except KeyboardInterrupt:
-        print(" Gagal Memproses data")
+        print(" Program is Closed By User")
+        print("==========================\n")
+        print("Thanks For Using My Program\n")
+        print("===========================\n")
+        userinput = input("Use Again? [Y] :").upper()
+        os.system("clear")
+        if userinput == "Y":
+            memixBot()
+        else:
+            print("==========================\n")
+            print("Thanks For Using My Program\n")
+            print("===========================\n")
+            return
     finally:
         mt5.shutdown()
 
 
 if __name__ == "__main__":
-    bot()
+    memixBot()
